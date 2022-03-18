6d7edb1d583759cb683c9978aeed4c628b764d11
law101 <lawrencebolu@gmail.com>
2022-03-18 09:17:01 +0100

    Created Predict script

--- churn/predict.py
+++ churn/predict.py
@@ -1,6 +1,15 @@
 from pysurvival.utils import load_model
     
-def predict(X, artifact_path):
+def get_prediction(X, artifact_path):
+    """_summary_
+
+    Args:
+        X (_type_): _description_
+        artifact_path (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     # Load artifact
     model = load_model(artifact_path)
     # compute every customer's survival function and risk score for 4 chosen periods (active months)

1282a011ae219915599b28d54265acc70f2b4694
Lawrence Okegbemi <lawrencebolu@gmail.com>
2022-05-15 19:24:04 +0100

    Added Project Base function

--- churn/main.py
+++ churn/main.py
@@ -4,26 +4,27 @@ from pysurvival.models.survival_forest import ConditionalSurvivalForestModel
 import train
 import preprocess
 
-artifact_path = Path('/mnt/c/Users/Lawrence/Downloads/Lawrence/survival-analysis/artifact/model.zip')
+# Define project base directory
+def get_project_root() -> Path:
+    return Path(__file__).absolute().parent.parent
 
-def training(data):
+
+def training(data, artifact_path):
     encoded_data = preprocess.one_hot_encoder(data)
     X_train, T_train, E_train, X_test, T_test, E_test = preprocess.data_splitter(encoded_data)
-
     # Define Model
     model = ConditionalSurvivalForestModel(num_trees=200)
-
     trainer = train.Trainer(model)
-    trainer.train_step(X_train, T_train, E_train)
-
+    trainer.train_step(X_train, T_train, E_train, artifact_path)
     # Evaluation
-    scores = trainer.eval_step(artifact_path=artifact_path, X=X_test, T=T_test, E=E_test)
-
+    scores = trainer.eval_step(X=X_test, T=T_test, E=E_test, artifact_path=artifact_path)
     return scores
 
-data_path = Path('/mnt/c/Users/Lawrence/Downloads/Lawrence/survival-analysis/data/customer_churn.csv')
-print(data_path)
 
-data = pd.read_csv(data_path)
+if __name__ == '__main__':
 
-training(data)
+    PROJECT_BASE = get_project_root()
+    artifact_path = Path.joinpath(PROJECT_BASE, "artifact/model.zip")
+    data_path = Path.joinpath(PROJECT_BASE, "data/customer_churn.csv")
+    data = pd.read_csv(data_path)
+    training(data, artifact_path)

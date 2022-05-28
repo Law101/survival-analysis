3f303632e132b56493247777ae6f881d7fc00053
Lawrence Okegbemi <lawrencebolu@gmail.com>
2022-05-28 21:19:38 +0100

    Added Setup for softwares

--- terraform/initial_setup.sh
+++ terraform/initial_setup.sh
@@ -2,6 +2,7 @@
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y nginx
 
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
